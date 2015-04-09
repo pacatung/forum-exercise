a6427803083c5f781a83f40b57318c120d4a28bf
BigBoss <pacatung@gmail.com>
2015-04-09 16:59:56 +0800

    modify by ihower

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,10 +3,7 @@ class MapsController < ApplicationController
 
   before_action :set_map, :only => [:show, :edit, :update, :destroy]
 
-
   def index
-    @maps = Map.all
-    #gem kaminari
     @maps = Map.page(params[:page]).per(2)
   end
 
@@ -17,51 +14,40 @@ class MapsController < ApplicationController
   def create
     @map = Map.new(map_params)
 
-#    @map.save
     if @map.save
-      redirect_to maps_path, :method =>:get
+      flash[:notice] = "Map was successfully created"
+      redirect_to maps_path
     else
       render :action => :new
     end
-
-    #redirect_to maps_path, :method => :get
-    flash[:notice] = "Map was successfully created"
   end
 
   def show
     @page_title = @map.name
-    # @map = Map.find(params[:id])
-
-    # redirect_to :url => map_path, :method => :get
   end
 
   def edit
-    # @map = Map.find(params[:id])
   end
 
   def update
-    # @map = Map.find(params[:id])
-
-    #@map.update(map_params)
     if @map.update(map_params)
-      redirect_to map_path, :method =>:get
+      flash[:notice] = "Map was successfully updated"
+      redirect_to map_path
     else
       render :action => :edit
     end
-
-    #redirect_to map_path, :method => :get
-    flash[:notice] = "Map was successfully updated"
   end
 
   def destroy
-    # @map = Map.find(params[:id])
     @map.destroy
 
-    redirect_to maps_path, :method => :get
     flash[:alert] = "Map was successfully deleted"
+
+    redirect_to maps_path
   end
 
-private
+  private
+
   def map_params
     params.require(:map).permit(:name, :country, :time, :money, :days, :description)
   end
