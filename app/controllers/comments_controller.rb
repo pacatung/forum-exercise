9d1e047c142787efff77749bdb9e46399b03a6ab
BigBoss <pacatung@gmail.com>
2015-04-15 15:19:57 +0800

    complete create function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,42 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :set_map
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @map.comments.new(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      redirect_to map_url(@map)
+    else
+      render :template => "maps/show"
+    end
+  end
+
+  def destroy
+    @comment = @map.comments.find( params[:id] )
+
+    if @comment.can_delete_by?(current_user)
+      @comment.destroy
+    end
+
+    redirect_to :back
+
+  end
+
+
+  protected
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_map
+    @map = Map.find(params[:map_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :maps
+  resources :maps do
+    resources :comments
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
